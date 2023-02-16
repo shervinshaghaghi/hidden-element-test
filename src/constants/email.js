6dc57bb0f5a6af29a88e657e950a533c0515c37f
shervinshaghaghi <shervinshaghaghi@gmail.com>
2023-02-16 04:59:31 -0800

    fix: new email config

--- src/constants/email.js
+++ src/constants/email.js
@@ -1,5 +1,5 @@
 export const EMAIL = {
-  SERVICE_ID: 'service_s41e7hm',
-  TEMPLATE_ID: `template_o8o1k6p`,
-  PUBLIC_KEY: 't8tizxFfnZa1qNGmP'
+  SERVICE_ID: 'service_ztiwkju',
+  TEMPLATE_ID: `template_zuv3u3s`,
+  PUBLIC_KEY: '7jkXBTEF0Tgj5J_JX'
 };
